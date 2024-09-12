c979dc57db5bb4eb79c289c41e464bdb10fe5ac8
Michael Dooley Esquivel <x00198005@mytudublin.ie>
2024-09-12 20:20:50 +0100

    new links, css coloured homepage

--- webpages/urls.py
+++ webpages/urls.py
@@ -1,8 +1,13 @@
 # webpages/urls.py
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, AtlanticPageView, PacificPageView, IndianPageView, ArcticPageView, SouthernPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('atlantic/', AtlanticPageView.as_view(), name='atlantic'),
+    path('pacific/', PacificPageView.as_view(), name='pacific'),
+    path('indian/', IndianPageView.as_view(), name='indian'),
+    path('arctic/', ArcticPageView.as_view(), name='arctic'),
+    path('southern/', SouthernPageView.as_view(), name='southern')
 ]
--- webpages/views.py
+++ webpages/views.py
@@ -6,3 +6,18 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class AtlanticPageView(TemplateView):
+    template_name = 'atlantic.html'
+
+class PacificPageView(TemplateView):
+    template_name = 'pacific.html'
+
+class IndianPageView(TemplateView):
+    template_name = 'indian.html'
+
+class ArcticPageView(TemplateView):
+    template_name = 'arctic.html'
+
+class SouthernPageView(TemplateView):
+    template_name = 'southern.html'
